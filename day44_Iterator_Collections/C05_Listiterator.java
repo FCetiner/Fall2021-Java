98c0218c3808384fec49cc5c7870437f8a54adf8
Ferhat <fcetinerr@gmail.com>
2021-12-18 00:30:23 +0300

    42. gun 2

--- day44_Iterator_Collections/C05_Listiterator.java
+++ day44_Iterator_Collections/C05_Listiterator.java
@@ -2,6 +2,7 @@ package day44_Iterator_Collections;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class C05_Listiterator {
 
@@ -14,6 +15,18 @@ public class C05_Listiterator {
 		liste.add("Ayþe");
 		liste.add("Fatma");
 		liste.add("Emrah");
+		System.out.println(liste);//[Ali, Veli, Ayþe, Fatma, Emrah]
+		
+		ListIterator li1 = liste.listIterator(); //interfae dir
+		String temp="";
+		
+		
+		while (li1.hasNext()) {		
+			temp=(String) li1.next();
+			li1.set(temp+"x");
+		//	li1.set(li1.next()+"x");
+			
+		}
 		System.out.println(liste);
 	}
 
